06f37b3082ced459a2a14a209ae62061fb68685d
Raffael2323 <118739446+Raffael2323@users.noreply.github.com>
2023-05-26 11:17:38 -0300

    up

--- app.js
+++ app.js
@@ -4,20 +4,22 @@ const marte = document.querySelector('.marte');
 const terra = document.querySelector('.terra');
 const venus = document.querySelector('.venus');
 const plutao = document.querySelector('.plutao');
+const btn = document.querySelector('.btn');
 
 
 const imagensPlanetas = ["/imagens/mars.jpg","/imagens/earth.jpg", "/imagens/venus.jpg", "/imagens/saturno.jpg", "/imagens/plutao.jpg"];
 
 
-marte.addEventListener('click',   mudandoImagemPlanetaMars);
-terra.addEventListener('click',   mudandoImagemPlanetaTerra)
-venus.addEventListener('click',   mudandoImagemPlanetaVenus)
-plutao.addEventListener('click',  mudandoImagemPlanetaPlutao)
+    marte.addEventListener('click',   mudandoImagemPlanetaMars);
+    terra.addEventListener('click',   mudandoImagemPlanetaTerra)
+    venus.addEventListener('click',   mudandoImagemPlanetaVenus)
+    plutao.addEventListener('click',  mudandoImagemPlanetaPlutao)
 
 
 function mudandoImagemPlanetaMars () {
     img.setAttribute("src", imagensPlanetas[0])
 }
+
 function mudandoImagemPlanetaTerra () {
     img.setAttribute("src", imagensPlanetas[1])
 }
