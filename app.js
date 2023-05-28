9c986e5680f0c2f23834b73bcace39d77133abde
Raffael2323 <118739446+Raffael2323@users.noreply.github.com>
2023-05-27 23:23:14 -0300

    up

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const terra = document.querySelector('.terra');
 const venus = document.querySelector('.venus');
 const plutao = document.querySelector('.plutao');
 const btn = document.querySelector('.btn');
+const div = document.querySelector('.pandora');
 
 
 const imagensPlanetas = ["/imagens/mars.jpg","/imagens/earth.jpg", "/imagens/venus.jpg", "/imagens/saturno.jpg", "/imagens/plutao.jpg"];
@@ -21,7 +22,9 @@ function mudandoImagemPlanetaMars () {
 }
 
 function mudandoImagemPlanetaTerra () {
-    img.setAttribute("src", imagensPlanetas[1])
+    img.setAttribute("src", imagensPlanetas[1]);
+    div.innerHTML =  `<iframe width="560" height="315" src="https://www.youtube.com/embed/63IMreFMW4s?controls=0&amp;start=5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+
 }
 function mudandoImagemPlanetaVenus () {
     img.setAttribute("src", imagensPlanetas[2]) 
